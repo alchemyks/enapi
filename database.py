d37806e36c0af113cf9f8c66d9577a0464d3f549
alchimikus <drizer84@gmail.com>
2022-12-07 16:01:21 +0200

    Create db table

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 import sqlite3
 from sqlite3 import Error
 
+
+#https://www.sqlitetutorial.net/
+
+
+
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by db_file
@@ -30,18 +35,38 @@ def create_table(conn, create_table_sql):
 
 
 sql_create_table_street = """ CREATE TABLE IF NOT EXISTS  streets(
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL
+                                    street_id INTEGER PRIMARY KEY,
+                                    name TEXT NOT NULL
                                 ); """
 
-sql_create_table_houses = """ CREATE TABLE IF NOT EXISTS houses(
-                                id integer PRYMARY KEY,
-                                number text NOT NULL,
-                                lat text,
-                                lng text
-                                id_street FOREING KEY 
-                            ); """
+sql_create_table_houses = """CREATE TABLE IF NOT EXISTS houses(
+                                house_id   INTEGER PRIMARY KEY,
+                                number     TEXT    NOT NULL,
+                                lat        TEXT,
+                                lng        TEXT,
+                                street_id INTEGER NOT NULL,
+                                FOREIGN KEY (street_id)
+                                REFERENCES streets (street_id) 
+                                );"""
+
+gr = """CREATE TABLE supplier_groups (
+	group_id integer PRIMARY KEY,
+	group_name text NOT NULL
+);"""
+
+supp = """CREATE TABLE houses(
+    house_id   INTEGER PRIMARY KEY,
+    number     TEXT    NOT NULL,
+    lat        TEXT,
+    lng        TEXT,
+    street_id INTEGER NOT NULL,
+    FOREIGN KEY (street_id)
+       REFERENCES streets (street_id) 
+);"""
 
 db_con = create_connection('db/energy_api.db')
 create_table(db_con, sql_create_table_street)
+create_table(db_con, sql_create_table_houses)
+#create_table(db_con, gr)
+#create_table(db_con, supp)
 db_con.close()
